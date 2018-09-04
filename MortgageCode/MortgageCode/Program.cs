93204e8849680102401f3825eead7fe06e481438
Joseph <josephcrusso1993@gmail.com>
2018-09-04 01:08:01 -0400

    auto number set  and basic set up

---
+++ MortgageCode/MortgageCode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace MortgageCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrmServiceClient client = new CrmServiceClient("Url=https://finalproject.crm.dynamics.com; Username=Jrusso@finalproject.onmicrosoft.com; Password=Hpesoj93; authtype=Office365");
+            IOrganizationService service = (IOrganizationService)client.OrganizationWebProxyClient != null ? (IOrganizationService)client.OrganizationWebProxyClient : (IOrganizationService)client.OrganizationServiceProxy;
+
+
+            #region MortgageNumber
+            //CreateAttributeRequest widgetSerialNumberAttributeRequest = new CreateAttributeRequest
+            //{
+            //    EntityName = "project_mortgage",
+            //    Attribute = new StringAttributeMetadata
+            //    {
+            //        //Define the format of the attribute
+            //        AutoNumberFormat = "{ DATETIMEUTC:yyyymm}-{ SEQNUM: 6}",
+            //        LogicalName = "project_mortgagenumberA",
+            //        SchemaName = "project_mortgagenumberA",
+            //        RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None),
+            //        MaxLength = 12, // The MaxLength defined for the string attribute must be greater than the length of the AutoNumberFormat value, that is, it should be able to fit in the generated value.
+            //        DisplayName = new Label("Mortgage Number", 1033),
+            //        Description = new Label("Mortgage Number of the Account.", 1033)
+            //    }
+            //};
+            //service.Execute(widgetSerialNumberAttributeRequest);
+            #endregion
+
+        }
+    }
+}
---
+++ MortgageCode/MotgageExtension/Mortgage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace MotgageExtension
+{
+    public class idk:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            #region Setup
+            // Extract the tracing service for use in debugging sandboxed plug-ins.
+            // If you are not registering the plug-in in the sandbox, then you do
+            // not have to add any tracing service related code.
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the organization service reference which you will need for
+            // web service calls.
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            #endregion
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity Mortgage = (Entity)context.InputParameters["Target"];
+               
+
+                try
+                {
+                    //// Using Query by Attribute
+                    //QueryByAttribute query = new QueryByAttribute();
+                    //query.EntityName = "new_configuration";
+                    //query.ColumnSet = new ColumnSet(new string[] { "new_value" });
+                    //query.AddAttributeValue("new_tax", "Federal Tax");
+                   
+
+
+
+                  
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }//end if 
+        }//end execute
+    }
+}
