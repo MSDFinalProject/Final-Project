6c0fea57bddb0a5a62c705d5155e5407bfe9b7e9
Harry Cortes <heri032095@gmail.com>
2018-09-08 14:06:03 -0400

    Added two Javascript files
    Files for Validation (validation.js) and a file for AJAX calls (ajax.js)

---
+++ FinalProject/FinalProject/Javascript/AJAXCalls.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    $("#form").submit(onSubmit);
+});
+
+function onSubmit(e) {
+    e.preventDefault();
+    //debugger;
+    var q = buildQuery();
+    console.log(q);
+    sendPost(q);
+}
+
+function buildQuery() {
+    var newQuery = {
+        "query": document.querySelector("#fname").value + ", " + document.querySelector("#lname").value + " " + document.querySelector("#email").value + " " + document.querySelector("#msg").value
+    }
+
+    return newQuery;
+    //sendPost(newPerson);
+}
+
+function sendPost(newQuery) {
+    var url = "https://contactwebapp.azurewebsites.net/API/Person"/*My api*/;
+    var query = newQuery;
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: query,
+        dataType: "JSON",
+        success: function (response) {
+            if (response != null) {
+                para.text(JSON.stringify(response));
+            }
+            else {
+                alert("error");
+            }
+        }
+    })
+}   
---
+++ FinalProject/FinalProject/Javascript/Validation.js
@@ -0,0 +1,65 @@
+﻿function CanadaVal() {
+    if (document.getElementById("inputRegion").value == "Canada") {
+        document.getElementById("inputState").value = "";
+        document.getElementById("inputState").disabled = true;
+    }
+    else {
+        document.getElementById("inputState").disabled = false;
+    }
+}
+
+
+function SSNVal() {
+    if (!/^[0-9]+$/.test(document.querySelector("#inputSSN").value)) {
+        document.querySelector("#inputSSN").value = "";
+        document.querySelector("#inputSSN").setCustomValidity("SSN must be numeric.");
+    }
+    else {
+        document.querySelector("#inputSSN").setCustomValidity("");
+    }
+}
+
+function NameVal() {
+    if (/^[0-9]+$/.test(document.querySelector("#inputFirstName").value)) {
+        document.querySelector("#inputFirstName").value = "";
+        document.querySelector("#inputFirstName").setCustomValidity("Name must only contain letters.");
+    }
+    else {
+        document.querySelector("#inputFirstName").setCustomValidity("");
+    }
+
+    if (/^[0-9]+$/.test(document.querySelector("#inputLastName").value)) {
+        document.querySelector("#inputLastName").value = "";
+        document.querySelector("#inputLastName").setCustomValidity("Name must only contain letters.");
+    }
+    else {
+        document.querySelector("#inputLastName").setCustomValidity("");
+    }
+}
+
+function ZipVal() {
+    if (!/^[0-9]+$/.test(document.querySelector("#inputZip").value)) {
+        document.querySelector("#inputZip").value = "";
+        document.querySelector("#inputZip").setCustomValidity("Zip Code must be numeric.");
+    }
+    else {
+        document.querySelector("#inputZip").setCustomValidity("");
+    }
+}
+
+function MortgageAmountVal() {
+    if (!/^[0-9]+$/.test(document.querySelector("#inputMortgageAmount").value)) {
+        document.querySelector("#inputMortgageAmount").value = "";
+        document.querySelector("#inputMortgageAmount").setCustomValidity("Mortgage Amount must be numeric.");
+    }
+    else {
+        document.querySelector("#inputMortgageAmount").setCustomValidity("");
+    }
+
+    if (document.querySelector("#inputMortgageAmount").value < "50000"){
+        document.querySelector("#inputMortgageAmount").setCustomValidity("Mortgage Amount must be greater than $50,000.");
+    }
+    else {
+        document.querySelector("#inputMortgageAmount").setCustomValidity("");
+    }
+}
